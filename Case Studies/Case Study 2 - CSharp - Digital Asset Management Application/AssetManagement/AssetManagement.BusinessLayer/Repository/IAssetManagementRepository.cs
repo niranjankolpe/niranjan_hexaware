9c62acd24b53b4e2dfdbba53a8735c321bb3f87c
DESKTOP-55ISL9F\niran <niranjankolpe@gmail.com>
2024-10-14 16:29:12 +0530

    Asset Management with Folder Structure

---
+++ Case Studies/Case Study 2 - CSharp - Digital Asset Management Application/AssetManagement/AssetManagement.BusinessLayer/Repository/IAssetManagementRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssetManagement;
+
+namespace AssetManagement
+{
+    public interface IAssetManagementRepository
+    {
+        bool AddAsset(Assets asset);
+
+        bool UpdateAsset(Assets asset);
+
+        bool DeleteAsset(int assetID);
+
+        bool AllocateAsset(int assetID, int employeeID, string allocationDate);
+
+        bool DeallocateAsset(int assetID, int employeeID, string returnDate);
+
+        bool PerformMaintenance(int assetID, string maintenanceDate, string description, float cost);
+
+        bool ReserveAsset(int assetID, int employeeID, string reservationDate, string startDate, string endDate);
+
+        bool WithdrawReservation(int reservationID);
+    }
+}
